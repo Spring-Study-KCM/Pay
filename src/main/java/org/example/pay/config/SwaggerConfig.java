99d74996b33b3ac3739739344498aea77f9bcbb2
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-05 17:25:23 +0900

    [feat] #1 Swagger 설정 클래스 생성

---
+++ src/main/java/org/example/pay/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package org.example.pay.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Pay Swagger")
+                .description("Pay API Docs")
+                .version("1.0.0");
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .components(new Components())
+                .info(info);
+    }
+}
