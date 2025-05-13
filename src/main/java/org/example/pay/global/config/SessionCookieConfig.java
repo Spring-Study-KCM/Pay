29300f661c9be04668a015d25a0ad0263577bc93
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-13 09:30:50 +0900

    [feat] #12 session을 저장 할 쿠키 발급 config

---
+++ src/main/java/org/example/pay/global/config/SessionCookieConfig.java
@@ -0,0 +1,21 @@
+package org.example.pay.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class SessionCookieConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("SESSIONID");
+        serializer.setCookiePath("/");
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setUseSecureCookie(false);
+        
+        return serializer;
+    }
+}
