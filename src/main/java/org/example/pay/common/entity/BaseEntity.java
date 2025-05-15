782f02ca615bf23249a52078322d2f4d79071498
cherryiJuice <chldlwn711@gmail.com>
2025-05-15 11:26:41 +0900

    [feat] 베이스 엔티티 추가

---
+++ src/main/java/org/example/pay/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package org.example.pay.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+public abstract class BaseEntity {
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	private LocalDateTime updatedDate;
+}
