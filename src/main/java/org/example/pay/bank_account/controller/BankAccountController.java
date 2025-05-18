180284d98f0f100e0d4541dcad80924ab3299c93
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-19 00:03:50 +0900

    [feat] #22 페이 계좌와 연결된 금융 계좌 조회

--- src/main/java/org/example/pay/bank_account/controller/BankAccountController.java
+++ src/main/java/org/example/pay/bank_account/controller/BankAccountController.java
@@ -4,11 +4,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.example.pay.auth.dto.request.UserSessionId;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.example.pay.bank_account.dto.request.BackAccountRequest;
 import org.example.pay.bank_account.dto.request.DisconnectAccountRequest;
+import org.example.pay.bank_account.dto.response.BankAccountInfoResponse;
+import org.example.pay.bank_account.service.BankAccountService;
+import org.example.pay.global.dto.ResponseDto;
+import org.example.pay.global.service.CustomUserDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +24,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Bank Account Controller", description = "페이 계좌 충전 및 금융 계좌 연동 관련 API 입니다.")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/account/bank")
 public class BankAccountController {
+
+    private final BankAccountService bankAccountService;
 
     @Operation(summary = "페이 계좌 연결 API")
     @ApiResponses(value = {
@@ -35,8 +44,13 @@ public class BankAccountController {
             @ApiResponse(responseCode = "200", description = "페이와 연결된 계좌 목록들입니다."),
     })
     @GetMapping("/connect")
-    public ResponseEntity<Object> connectedBankAccount(@RequestBody final UserSessionId userSessionId) {
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+    public ResponseEntity<ResponseDto<List<BankAccountInfoResponse>>> connectedBankAccount(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        List<BankAccountInfoResponse> connectedBankAccount = bankAccountService.getConnectedBankAccount(
+                customUserDetails.getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success(connectedBankAccount));
     }
 
     @Operation(summary = "페이와 연결된 금융 계좌 삭제 API")
