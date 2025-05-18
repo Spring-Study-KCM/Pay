ef38a483c5f903417516f467740b798e20050e65
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-18 23:39:58 +0900

    [fix] #24 인증된 사용자 정보(Authentication 객체)를 SecurityContext에 저장하고 stateless 방식이기에 다음 요청을 위해 현재 세션 저장

--- src/main/java/org/example/pay/global/handler/AuthenticationSuccessHandler.java
+++ src/main/java/org/example/pay/global/handler/AuthenticationSuccessHandler.java
@@ -4,20 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-import org.example.pay.global.dto.ResponseDto;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.example.pay.global.dto.ResponseDto;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.stereotype.Component;
 
-/**
- * 인증 성공 시 ResponseDto 형식의 JSON 응답을 반환하는 핸들러
- */
+
 @Component
 @RequiredArgsConstructor
 public class AuthenticationSuccessHandler implements
@@ -28,20 +26,15 @@ public class AuthenticationSuccessHandler implements
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        HttpSession session = request.getSession();
-        String sessionId = session.getId();
 
-        String roles = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(authentication);
 
-        session.setAttribute("CURRENT_USER", authentication.getName());
+        HttpSession session = request.getSession(true);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, context);
 
         Map<String, Object> data = new HashMap<>();
         data.put("message", "로그인 성공");
-        data.put("username", authentication.getName());
-        data.put("roles", roles);
-        data.put("sessionId", sessionId);
 
         ResponseDto<Map<String, Object>> responseDto = ResponseDto.success(data);
 
