aba6954b2f2b5b660ae64ea73dedcbf33d09b045
bronze_yun_4521 <ehddbs452100@naver.com>
2025-05-19 09:30:46 +0900

    [feat] #22 페이계좌와 금융계좌 연결

--- src/main/java/org/example/pay/bank_account/service/BankAccountService.java
+++ src/main/java/org/example/pay/bank_account/service/BankAccountService.java
@@ -4,17 +4,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.example.pay.bank_account.domain.BankAccount;
+import org.example.pay.bank_account.dto.request.BankType;
 import org.example.pay.bank_account.dto.response.BankAccountInfoResponse;
 import org.example.pay.bank_account.repository.BankAccountRepository;
 import org.example.pay.global.exception.PayException;
 import org.example.pay.global.exception.code.BankAccountErrorCode;
+import org.example.pay.pay_account.domain.PayAccount;
+import org.example.pay.pay_account.service.PayAccountService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BankAccountService {
 
     private final BankAccountRepository bankAccountRepository;
+    private final PayAccountService payAccountService;
 
     public List<BankAccountInfoResponse> getConnectedBankAccount(Long id) {
 
@@ -32,4 +38,24 @@ public class BankAccountService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void connectBankAccount(Long userId, String bankName, String bankAccountNumber) {
+
+        PayAccount payAccount = payAccountService.getPayAccount(userId);
+
+        BankType bankType = validateBankName(bankName);
+
+        BankAccount bankAccount = BankAccount.builder()
+                .accountNumber(bankAccountNumber)
+                .bankType(bankType)
+                .payAccount(payAccount)
+                .user(payAccount.getUser())
+                .build();
+
+        bankAccountRepository.save(bankAccount);
+    }
+
+    private BankType validateBankName(String bankName) {
+        return BankType.getBankType(bankName);
+    }
 }
--- src/main/java/org/example/pay/pay_account/repository/PayAccountRepository.java
+++ src/main/java/org/example/pay/pay_account/repository/PayAccountRepository.java
@@ -1,7 +1,10 @@
 package org.example.pay.pay_account.repository;
 
+import java.util.Optional;
 import org.example.pay.pay_account.domain.PayAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PayAccountRepository extends JpaRepository<PayAccount, Long> {
+
+    Optional<PayAccount> findByUserId(Long userId);
 }
