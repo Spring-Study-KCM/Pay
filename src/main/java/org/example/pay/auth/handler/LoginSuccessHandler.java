efb939eee43451c5fd3600b138d4512b459a9474
cherryiJuice <chldlwn711@gmail.com>
2025-05-20 14:53:45 +0900

    [refactor] security context service 재사용 및 메서드로 분리

--- src/main/java/org/example/pay/auth/handler/LoginSuccessHandler.java
+++ src/main/java/org/example/pay/auth/handler/LoginSuccessHandler.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.example.pay.auth.service.SecurityContextService;
+import org.example.pay.member.domain.Member;
+import org.example.pay.member.service.MemberService;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,17 +24,22 @@ import lombok.RequiredArgsConstructor;
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
 	private final ObjectMapper objectMapper;
+	private final MemberService memberService;
+	private final SecurityContextService securityContextService;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 		Authentication authentication) throws IOException, ServletException {
-		SecurityContext context = SecurityContextHolder.getContext();
-		context.setAuthentication(authentication);
-		SecurityContextHolder.setContext(context);
+		String email = authentication.getName();
+		Member member = memberService.getByEmail(email);
 
 		HttpSession session = request.getSession(true);
-		session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, context);
+		securityContextService.saveSecurityContext(session, member);
 
+		makeResponse(response, session);
+	}
+
+	private void makeResponse(HttpServletResponse response, HttpSession session) throws IOException {
 		Map<String, Object> data = new HashMap<>();
 		data.put("message", "로그인 성공");
 		data.put("sessionId", session.getId());
