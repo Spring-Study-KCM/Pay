1c9cf0fed9092544fd9ebc858512578d5cde868a
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-23 14:40:40 +0900

    [refactor] #40 에러 코드 주석 설명 변경

--- src/main/java/org/example/pay/global/exception/code/AuthErrorCode.java
+++ src/main/java/org/example/pay/global/exception/code/AuthErrorCode.java
@@ -8,15 +8,15 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AuthErrorCode implements DefaultErrorCode{
 
-    //400 BAD_REQUEST
+    //400
     VERIFICATION_INFO_NULL(HttpStatus.BAD_REQUEST, "인증 정보가 없습니다."),
     VERIFICATION_CODE_WRONG(HttpStatus.BAD_REQUEST, "인증번호가 틀렸습니다."),
     EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "이메일 인증이 완료되지 않았습니다."),
 
-    // 401 UNAUTHORIZED
+    // 401
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 잘못되었습니다."),
 
-    // 409 CONFLICT
+    // 409
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
     ;
 
--- src/main/java/org/example/pay/global/exception/code/MailErrorCode.java
+++ src/main/java/org/example/pay/global/exception/code/MailErrorCode.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MailErrorCode implements DefaultErrorCode{
 
-    //400 BAD_REQUEST
+    //400
     MISSING_EMAIL(HttpStatus.BAD_REQUEST, "이메일 주소가 누락되었습니다."),
 
-    //500 INTERNAL_SERVER_ERROR
+    //500
     EMAIL_SENDING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 발송에 실패했습니다."),
     ;
 
