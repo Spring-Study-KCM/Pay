4c93ad32b54f73fa8c53e85c3b2979a15b718f34
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-23 14:41:21 +0900

    [feat] #40 거래 내역 엔티티 추가

---
+++ src/main/java/org/example/pay/account/pay_account/repository/TransactionsRepository.java
@@ -0,0 +1,7 @@
+package org.example.pay.account.pay_account.repository;
+
+import org.example.pay.account.pay_account.domain.Transactions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionsRepository extends JpaRepository<Transactions, Long> {
+}
