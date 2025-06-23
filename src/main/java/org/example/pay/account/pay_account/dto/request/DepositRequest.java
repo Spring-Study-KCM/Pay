57b9f98a24fcd0c2d495459238ba0655555cf433
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-23 14:53:49 +0900

    [feat] #40 페이 금액 받

--- src/main/java/org/example/pay/account/pay_account/dto/request/DepositRequest.java
+++ src/main/java/org/example/pay/account/pay_account/dto/request/DepositRequest.java
@@ -1,4 +1,4 @@
 package org.example.pay.account.pay_account.dto.request;
 
-public record DepositRequest(Long friendId, Long amount) {
+public record DepositRequest(Long transactionsId, Long friendId, Long amount) {
 }
